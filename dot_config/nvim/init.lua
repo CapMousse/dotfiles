c6b5a020d59aa494247949000aade966a3f90394
Mewsse <echo@jeremy.sh>
2024-06-24 21:44:05 +0200

    Update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -138,7 +138,11 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup 'plugins'
+require('lazy').setup('plugins', {
+  rocks = {
+    enabled = false,
+  },
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
+    cmd = 'Neotree',
     branch = 'v3.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -12,6 +13,27 @@ return {
         '<C-n>',
         ':Neotree filesystem reveal left<cr>',
         desc = 'Open Neotree',
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+        callback = function()
+          local f = vim.fn.expand '%:p'
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd('Neotree current dir=' .. f)
+            vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+          end
+        end,
+      })
+    end,
+    config = function()
+      vim.g.loaded_netrwPlugin = 1
+      vim.g.loaded_netrw = 1
+    end,
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = 'open_current',
       },
     },
   },
