c85f9cd60bae3a576399512288535f8079af83fc
Mewsse <echo@jeremy.sh>
2024-06-25 18:22:53 +0200

    format + cleaup

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		cmd = "Neotree",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		keys = {
-			{
-				"<C-n>",
-				":Neotree filesystem toggle reveal left<cr>",
-				desc = "Open Neotree",
-			},
-		},
-		config = function()
-			vim.g.loaded_netrwPlugin = 1
-			vim.g.loaded_netrw = 1
-		end,
-		opts = {
-			filesystem = {
-				hijack_netrw_behavior = "open_current",
-			},
-		},
-	},
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    cmd = 'Neotree',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    keys = {
+      {
+        '<C-n>',
+        ':Neotree filesystem toggle reveal left<cr>',
+        desc = 'Open Neotree',
+      },
+    },
+    config = function()
+      vim.g.loaded_netrwPlugin = 1
+      vim.g.loaded_netrw = 1
+    end,
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = 'open_current',
+      },
+    },
+  },
 }
